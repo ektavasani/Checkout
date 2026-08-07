613e8e82b8f545be08c0074da2d7030e838a2a0a
ektavasani <ektav.crawlapps@gmail.com>
2026-08-07 17:55:55 +0530

    Cart Transform update function

--- extensions/cart-transformer/src/cart_transform_run.js
+++ extensions/cart-transformer/src/cart_transform_run.js
@@ -17,13 +17,26 @@ const NO_CHANGES = {
  * @returns {CartTransformRunResult}
  */
 export function cartTransformRun(input) {
+
   const firstItem = input.cart.lines[0];
+
+
   return {
     operations: [
       {
         lineUpdate: {
           cartLineId:firstItem.id,
           title:"Hello From my function",
+          price:{
+            adjustment:{
+              fixedPricePerUnit:{
+                amount: 20,
+              }
+            }
+          },
+          image:{
+            url:"https://cdn.shopify.com/s/files/1/0979/8215/2979/files/theme_cover_image.jpg?v=1773046626"
+          }
         }
       }
     ],
